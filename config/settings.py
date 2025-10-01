38f6a4953cf682c5f4ed97a0808e199f59d05a6f
Learan-Canning <learancanning@mail.com>
2025-10-01 10:06:58 +0100

    add heroku setup and create manager superuser account

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0jdi(^7q6aa-7*+1gu#i)n+%w!_tq-$bm#97^##9ayd4wm@ql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com'] 
 
