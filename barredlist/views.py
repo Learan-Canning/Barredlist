bfa6c00c0424f02d975d016ea041eb3d163961b4
Learan-Canning <learancanning@mail.com>
2025-10-08 12:57:15 +0100

    add post_detail view

--- barredlist/views.py
+++ barredlist/views.py
@@ -8,3 +8,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "barredlist/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`barredlist.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`barredlist.Post`.
+
+    **Template:**
+
+    :template:`barredlist/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "barredlist/post_detail.html",
+        {"post": post},
+    )
