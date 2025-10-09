95e08a64f6219e7da31fe4beac4a3d4f854da1e3
Learan-Canning <learancanning@mail.com>
2025-10-09 22:23:00 +0100

    create app and collaboration request

--- barredlist/forms.py
+++ barredlist/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Comment, Reaction
+from .models import Comment, Reaction 
+
 
 # ...existing code...
 
@@ -15,3 +16,4 @@ class ReactionForm(forms.ModelForm):
         widgets = {
             'reaction_type': forms.RadioSelect(),
         }
+
